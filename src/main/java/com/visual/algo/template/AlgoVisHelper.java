6929957b27fc3f9cb6fbd8ea2bed05ef00cb5c73
xuery <zhuxuebin@sf-express.com>
2017-12-20 21:02:03 +0800

    template

---
+++ src/main/java/com/visual/algo/template/AlgoVisHelper.java
@@ -0,0 +1,97 @@
+package com.visual.algo.template;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+
+/**
+ * Created by 01368080 on 2017/12/19.
+ */
+public class AlgoVisHelper {
+
+    private AlgoVisHelper() {
+    }
+
+    public static final Color Red = new Color(0xF44336);
+    public static final Color Pink = new Color(0xE91E63);
+    public static final Color Purple = new Color(0x9C27B0);
+    public static final Color DeepPurple = new Color(0x673AB7);
+    public static final Color Indigo = new Color(0x3F51B5);
+    public static final Color Blue = new Color(0x2196F3);
+    public static final Color LightBlue = new Color(0x03A9F4);
+    public static final Color Cyan = new Color(0x00BCD4);
+    public static final Color Teal = new Color(0x009688);
+    public static final Color Green = new Color(0x4CAF50);
+    public static final Color LightGreen = new Color(0x8BC34A);
+    public static final Color Lime = new Color(0xCDDC39);
+    public static final Color Yellow = new Color(0xFFEB3B);
+    public static final Color Amber = new Color(0xFFC107);
+    public static final Color Orange = new Color(0xFF9800);
+    public static final Color DeepOrange = new Color(0xFF5722);
+    public static final Color Brown = new Color(0x795548);
+    public static final Color Grey = new Color(0x9E9E9E);
+    public static final Color BlueGrey = new Color(0x607D8B);
+    public static final Color Black = new Color(0x000000);
+    public static final Color White = new Color(0xFFFFFF);
+
+    //空心圆
+    public static void strokeCircle(Graphics2D g2d, int x, int y, int r) {
+        Ellipse2D circle = new Ellipse2D.Double(x - r, y - r, 2 * r, 2 * r);
+        g2d.draw(circle);
+    }
+
+    //实心圆
+    public static void fillCircle(Graphics2D g2d, int x, int y, int r) {
+        Ellipse2D circle = new Ellipse2D.Double(x - r, y - r, 2 * r, 2 * r);
+        g2d.fill(circle);
+    }
+
+    //空心矩形
+    public static void strokeRectangle(Graphics2D g2d, int x, int y, int w, int h) {
+        Rectangle2D rectangle = new Rectangle2D.Double(x, y, w, h);
+        g2d.draw(rectangle);
+    }
+
+    //实心矩阵
+    public static void fillRectangle(Graphics2D g2d, int x, int y, int w, int h) {
+        Rectangle2D rectangle = new Rectangle2D.Double(x, y, w, h);
+        g2d.fill(rectangle);
+    }
+
+    //设置画笔粗细
+    public static void setStroke(Graphics2D g2d, int strokeWidth) {
+        g2d.setStroke(new BasicStroke(strokeWidth, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+    }
+
+    //设置颜色
+    public static void setColor(Graphics2D g2d, Color color) {
+        g2d.setColor(color);
+    }
+
+    public static void sleep(int t) {
+        try {
+            Thread.sleep(t);
+        } catch (InterruptedException e) {
+            System.out.println("thread sleeping error...");
+        }
+    }
+
+    //设置画图背景图
+    public static void putImage(Graphics2D g2d, int x, int y, String imageUrl) {
+        ImageIcon icon = new ImageIcon(imageUrl);
+        Image image = icon.getImage();
+
+        g2d.drawImage(image, x, y, null);
+    }
+
+    public static void drawText(Graphics2D g, String text, int centerx, int centery) {
+        if (text == null)
+            throw new IllegalArgumentException("Text is null in drawText function!");
+
+        FontMetrics metrics = g.getFontMetrics();
+        int w = metrics.stringWidth(text);
+        int h = metrics.getDescent();
+        g.drawString(text, centerx - w / 2, centery + h);
+    }
+}
---
+++ src/main/java/com/visual/algo/template/AlgoVisualizer.java
@@ -0,0 +1,46 @@
+package com.visual.algo.template;
+
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.MouseAdapter;
+
+/**
+ * Created by 01368080 on 2017/12/20.
+ */
+public class AlgoVisualizer {
+
+    private AlgoFrame algoFrame;
+    private Object data;
+
+    public AlgoVisualizer(int screenWidth, int screenHeight, int N) {
+
+        //todo 初始化数据
+
+
+        //todo 初始化视图
+        EventQueue.invokeLater(() -> {
+            algoFrame = new AlgoFrame("welcome", screenWidth, screenHeight);
+            //todo 根据具体情况加入监听事件
+            algoFrame.addKeyListener(new AlgoKeyListener());
+            algoFrame.addMouseListener(new AlgoMouseListener());
+            new Thread(() -> {
+                run();
+            }).start();
+        });
+    }
+
+    //todo 动画逻辑
+    private void run() {
+        //todo 编写自己的动画逻辑
+    }
+
+    //添加一个键盘事件
+    private class AlgoKeyListener extends KeyAdapter {
+
+    }
+
+    //添加一个鼠标事件
+    private class AlgoMouseListener extends MouseAdapter {
+
+    }
+}
